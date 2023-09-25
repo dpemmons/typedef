5d0b1f3e71452b9c5b0c57e89f9702d6c7bd1220
Dale Emmons <dpemmons@gmail.com>
2023-09-25 15:09:06 -0700

    catch exceptions thrown during error reporting.

--- tests/parser/helpers.h
+++ tests/parser/helpers.h
@@ -12,25 +12,30 @@
 #include "libtypedef/parser/parser_error_info.h"
 #include "libtypedef/parser/typedef_parser.h"
 
-#define REQUIRE_NO_PARSE_ERROR(x)                                              \
-  CHECKED_ELSE(x == 0) {                                                       \
-    std::stringstream ss;                                                      \
-    ss << "Successful parse expected. Instead we got:\n";                      \
-    for (const auto& err : parser.Errors()) {                                  \
-      std::istringstream iss{parser.InputString()};                            \
-      ss << fmt::format("Line {}:{}: {} {}\n", err.line, err.line_offset,      \
-                        err.ErrorTypeToString(), err.message);                 \
-      std::string line;                                                        \
-      for (int l = 0; std::getline(iss, line); l++) {                          \
-        if (l == err.line - 1) {                                               \
-          ss << fmt::format("{}\n", line);                                     \
-          size_t len = err.length ? err.length : 1;                            \
-          ss << fmt::format("{: >{}}{:^>{}}\n", "", err.line_offset, "", len); \
-          break;                                                               \
-        }                                                                      \
-      }                                                                        \
-    }                                                                          \
-    FAIL(ss.str());                                                            \
+#define REQUIRE_NO_PARSE_ERROR(x)                                           \
+  CHECKED_ELSE(x == 0) {                                                    \
+    std::stringstream ss;                                                   \
+    ss << "Successful parse expected. Instead we got:\n";                   \
+    try {                                                                   \
+      for (const auto& err : parser.Errors()) {                             \
+        std::istringstream iss{parser.InputString()};                       \
+        ss << fmt::format("Line {}:{}: {} {}\n", err.line, err.line_offset, \
+                          err.ErrorTypeToString(), err.message);            \
+        std::string line;                                                   \
+        for (int l = 0; std::getline(iss, line); l++) {                     \
+          if (l == err.line - 1) {                                          \
+            ss << fmt::format("{}\n", line);                                \
+            size_t len = err.length ? err.length : 1;                       \
+            ss << fmt::format("{: >{}}{:^>{}}\n", "", err.line_offset, "",  \
+                              len);                                         \
+            break;                                                          \
+          }                                                                 \
+        }                                                                   \
+      }                                                                     \
+    } catch (...) {                                                         \
+      FAIL("Exception thrown while trying to print parse errors!");         \
+    }                                                                       \
+    FAIL(ss.str());                                                         \
   }
 
 #endif  // TESTS_HELPERS_H__
